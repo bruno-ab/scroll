0e7400176b5805a70ee8067632ccfcd1a9465fb1
Bruno Alves <brunolasombra@gmail.com>
2019-08-31 11:52:35 -0300

    set response

--- scroll/app/Controllers/Http/UserController.js
+++ scroll/app/Controllers/Http/UserController.js
@@ -1,27 +1,27 @@
 'use strict'
 const User = use('App/Models/User')
-const request = use('@adonisjs/framework/src/Request')
+const request = use('Adonis/Src/Request')
 const response = use('Adonis/Src/Response')
 
 class UserController {
-    async index () {
-      const users = User.all()  
-      console.log(response)
-      return new response(users)
+    async index ({ request, response }) {
+      const users = await User.all()  
+      // console.log(response)]
+     
+      return response.status(200).send(users)
       }
 
-      async save (request,response) {
+      async save ({ request, response }) {
         try{
           const user = new User()
-          user.username = 'John Doe'
-          user.password = '123456'
-          user.email = 'johndoe@gmail.com'
-          
-          await user.save()
-          return response.send(`Usuário salvo com sucesso`)
+          user.username = request.body.username
+          user.password = request.body.password
+          user.email = request.body.email
+          // await user.save()
+          return response.status(200).send(`Usuário salvo com sucesso`)
         }catch(err){
           console.log(err)
-          return response.send('Falha ao salvar')
+          return response.json('Falha ao salvar')
         }
 
       }

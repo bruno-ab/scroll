f6e3a25fadb1ffb87afbf664878c7a0746ed3134
Bruno Alves <brunolasombra@gmail.com>
2019-08-30 23:05:55 -0300

    updates

--- scroll/app/Controllers/Http/UserController.js
+++ scroll/app/Controllers/Http/UserController.js
@@ -1,18 +1,29 @@
 'use strict'
 const User = use('App/Models/User')
+const request = use('@adonisjs/framework/src/Request')
+const response = use('Adonis/Src/Response')
 
 class UserController {
-    async index (request, response) {
-        return await User.all()
+    async index () {
+      const users = User.all()  
+      console.log(response)
+      return new response(users)
       }
 
       async save (request,response) {
-        const user = new User()
-        user.username = 'John Doe'
-        user.password = '123456'
-        user.email = 'johndoe@gmail.com'
-        
-        await user.save()
+        try{
+          const user = new User()
+          user.username = 'John Doe'
+          user.password = '123456'
+          user.email = 'johndoe@gmail.com'
+          
+          await user.save()
+          return response.send(`Usuário salvo com sucesso`)
+        }catch(err){
+          console.log(err)
+          return response.send('Falha ao salvar')
+        }
+
       }
 }
 
--- scroll/start/routes.js
+++ scroll/start/routes.js
@@ -18,4 +18,4 @@ const Route = use('Route')
 
 Route.on('/').render('welcome')
 Route.get('/user','UserController.index')
-Route.get('/user/save','UserController.save')
+Route.post('/user','UserController.save')

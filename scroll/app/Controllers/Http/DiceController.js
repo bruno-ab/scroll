299441014d89d760ea786ed6a32c8b4c18bdd7d4
bruno alves botelho <bruno@ecobonuz.com>
2020-03-11 09:57:43 -0300

    success logic

--- scroll/app/Controllers/Http/DiceController.js
+++ scroll/app/Controllers/Http/DiceController.js
@@ -3,21 +3,28 @@
 class DiceController {
     async getDice ({ request, response, params }) {
         
-        // console.log(request)
+        const query = request.get()
         const minDice = Number(params.minDice)
         const maxDice = Number(params.maxDice)
         const number = Number(params.number)
         const dices = []
-
+        let success = 0
         for (let index = 0; index < number; index++) {
             // function to roll dices! good lucky
             const dice = await this.rollDice(minDice, maxDice, number)
             dices.push(dice)         
         }
         
+        if(query.cd) {
+            for(let dice of dices) {
+                if(dice >= query.cd) success++ 
+            }
+        }
+        
         const data = {
             dicesRolled: number,
-            result: dices
+            result: dices,
+            success: success
         }
         return response.json(data)
        
